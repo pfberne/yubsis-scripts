85fc7df73adf32ede037a058a26b630002f45332
Martin Lehoux <martin@lehoux.net>
2020-04-07 22:02:54 -0700

    feat - Example conf file

---
+++ conf.example.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+SERVER_NAME = "localhost"
+EMAILS = ['martin@lehoux.net']
+SENDER_EMAIL = "sender@email.com"
+SMTP_SERVER = "mail.ikoula.fr"
+BACKUP_ROOT_PROD = '/var/backups/odoo'
+BACKUP_ROOT_TEST = '/var/backups/odoo/test'
+DISK_PARTITIONS = ['/']
