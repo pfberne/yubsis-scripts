693d1072b69639df7983792c184406a86096e593
Martin Lehoux <martin@lehoux.net>
2020-04-07 19:08:55 -0700

    feat - Conf file

---
+++ conf.py
@@ -0,0 +1,7 @@
+SERVER_NAME = "localhost"
+EMAILS = ['martin@lehoux.net']
+SENDER_EMAIL = "biodoo-prod@yubsis.com"
+SMTP_SERVER = "mail.ikoula.fr"
+BACKUP_ROOT_PROD = '/Users/mlehoux/Documents/JCS/odoo-backup'
+BACKUP_ROOT_TEST = '/Users/mlehoux/Documents/JCS/odoo-backup-test'
+DISK_PARTITIONS = ['/', '/System/Volumes/Data']
