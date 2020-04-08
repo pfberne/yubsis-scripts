0c649aa3a9359ef4bda1222a2f6d858cd080de9b
Martin Lehoux <martin@lehoux.net>
2020-04-07 19:55:07 -0700

    feat - Export email in its file

---
+++ mail.py
@@ -0,0 +1,28 @@
+import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+from conf import BACKUP_ROOT_PROD, SERVER_NAME, SENDER_EMAIL, SMTP_SERVER, EMAILS
+
+
+class Email(MIMEMultipart):
+    backup_root = BACKUP_ROOT_PROD
+    title = None
+
+    def __init__(self, backup_root=BACKUP_ROOT_PROD):
+        super().__init__("alternative")
+        self.backup_root = backup_root
+        self.timestamp = datetime.datetime.now().replace(microsecond=0)
+        self['Subject'] = "{server} : {title} - {date}".format(
+            server=SERVER_NAME,
+            title=self.title,
+            date=self.timestamp
+        )
+        self['From'] = SENDER_EMAIL
+
+    def send(self, message):
+        client = smtplib.SMTP(SMTP_SERVER)
+        for email in EMAILS:
+            message['To'] = email
+            client.sendmail(SENDER_EMAIL, email, message.as_string())
+        client.quit()
