069299bd22caddbf5085d83fa0ccc9201d6f42b4
Martin Lehoux <martin@lehoux.net>
2020-04-07 22:02:54 -0700

    feat - Example conf file

---
+++ conf.example.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+SERVER_NAME = "localhost"
+EMAILS = ['martin@lehoux.net']
+SENDER_EMAIL = "biodoo-prod@yubsis.com"
+SMTP_SERVER = "mail.ikoula.fr"
+BACKUP_ROOT_PROD = '/var/backups/odoo'
+BACKUP_ROOT_TEST = '/var/backups/odoo/test'
+DISK_PARTITIONS = ['/']
--- conf.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-SERVER_NAME = "localhost"
-EMAILS = ['martin@lehoux.net']
-SENDER_EMAIL = "biodoo-prod@yubsis.com"
-SMTP_SERVER = "mail.ikoula.fr"
-BACKUP_ROOT_PROD = '/Users/mlehoux/Documents/JCS/odoo-backup'
-BACKUP_ROOT_TEST = '/Users/mlehoux/Documents/JCS/odoo-backup-test'
-DISK_PARTITIONS = ['/', '/System/Volumes/Data']
